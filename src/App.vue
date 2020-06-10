b7b62cdb0f26d451571d89c8e56313a8b4b36f9f
Mateo <joker1988_88@mail.com>
2020-06-10 21:47:38 +0600

    Top nav implement

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <TopNav/>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+import '@/assets/scss/bootstrap.scss';
 import TopNav     from '@/components/TopNav.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     TopNav
   }
 }
 </script>
 <style lang="sass" scoped>
-    @import '@/assets/scss/_utilities';
 </style>
