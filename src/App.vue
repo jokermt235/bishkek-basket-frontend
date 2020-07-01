df7d60af12d5c8303c529c165318d3e43682709a
mutalip <jokermt2357@gmail.com>
2020-07-01 16:17:14 +0600

    Adding bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@ import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-import '@/assets/scss/bootstrap.scss';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import TopNav     from '@/components/TopNav.vue';
 export default {
   name: 'App',
